f9f577727748b020dca9871dd14fb80e0cfa0ec1
Ankit Sharma <generalankit51@gmail.com>
2025-11-20 08:27:28 +0545

    new commit

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -12,11 +12,27 @@ export const books: Book[] = [
 ];
 
 export class BookController {
+    createBook = (req: Request, res: Response) => {
+        const { id, title } = req.body; // destructuring
+        // const id = req.body.id;
+        if (id) {
+            return res.status(400).json({ message: "Book ID is required" });
+        }
+        if (title) {
+            return res.status(400).json({ message: "Book title is required" });
+        }
+        const checkBook = books.find(book => book.id === id);
+        if (checkBook) {
+            return res.status(409).json({ message: "Book with this ID already exists" });
+        }
+        const newBook: Book = { id, title };
+        // same as { id: id, title: title }, if key and variable names are same
+        books.push(newBook);
+        return res.status(201).json(newBook);
+
+    }
     getBooks = (req: Request, res: Response) => {
-        const books = [
-            { id: 'B-1', title: '1984' },
-            { id: 'B-2', title: 'To kill a Mockingbird', date: '2015-12-10' }
-        ];
-        res.status(200).json(books);
+        const return_book: Book[] = books;
+        res.status(200).json(return_book);
     }
 }
